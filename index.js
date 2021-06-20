d64a02e45144e17035509a38b3ba3fc8e2e2b668
Thong Tuan Kiet <thongtuankiet.1999@gmail.com>
2021-06-20 21:14:21 +0700

    Add checked and delete task

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
-const dataString = localStorage.getItem("users");
-let users;
+const dataString = localStorage.getItem("tasks");
+let tasks;
 
 if (dataString) {
-  users = JSON.parse(dataString);
+  tasks = JSON.parse(dataString);
 } else {
-  users = [];
+  tasks = [];
 }
 
 function handleAdd() {
-  const input = document.getElementById("input-name");
+  const input = document.getElementById("input-task");
   const value = input.value;
   if (value === "") {
     alert("Please enter text");
     return;
   }
-  users.push(value);
+  tasks.push(value);
   input.value = "";
-  localStorage.setItem("users", JSON.stringify(users));
+  localStorage.setItem("tasks", JSON.stringify(tasks));
   render();
 }
 
+function handleDelItem(index) {
+  tasks.splice(index, 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  render();
+}
+
+function handleCheckedItem(index) {
+  const item = document.querySelector(`.far-${index}`);
+  item.classList.add("checked");
+}
+
 function render() {
-  const listUser = document.querySelector(".list-user");
-  const content = users.map((item) => {
-    return `<li>${item}</li>`;
+  const listUser = document.querySelector(".list-task");
+  const content = tasks.map((item, index) => {
+    return `<div class="task-item">
+      <button class="btn btn-checked" type="button" onclick=handleCheckedItem(${index})><i class="far fa-check-circle far-${index}"></i></button>
+      <li>${item}</li>
+      <button class="btn btn-delete" type="button" onclick=handleDelItem(${index})><i class="far fa-times-circle"></i></button>
+    </div>`;
   });
   listUser.innerHTML = content.join("");
 }
